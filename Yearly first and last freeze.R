5ac277c3f5d8ab2ed586253d617bd9d48832f5f3
Sabi Horvat <sabolch.horvat@gmail.com>
2022-11-19 07:55:13 -0800

    added 1871-1940 data from Portland regional weather station office

--- Yearly first and last freeze.R
+++ Yearly first and last freeze.R
@@ -13,7 +13,8 @@ library(ggtext)
 
 # GET DATA from https://www.climate.gov/maps-data/dataset/past-weather-zip-code-data-table
 # via online search https://www.ncdc.noaa.gov/cdo-web/search
-csv_data <- read_csv('data/97218_portland_1940_2022-11_temp_prec.csv') # 1941 January to 2022 November
+# csv_data <- read_csv('data/97218_portland_1940_2022-11_temp_prec.csv') # 1941 January to 2022 November
+csv_data <- read_csv('data/portland_1871_11_2022-11_temp_prec.csv') # 1871 January to 2022 November
 
 # for appending new data (added 2021-full year and 2020-December data)
 # sort_data <- csv_data %>%
@@ -47,13 +48,13 @@ last_freeze_yday <- last_freeze %>% mutate(yday = yday(max))
 # Note that the origin date doesn't matter, need date format for the x-axis scale to work
 freeze <- ggplot() +
   geom_point(data=freeze_days, aes(y=year, x=as.Date(yday,origin='2020-01-01'))
-             , color = "gray", size = 0.8)+
+             , color="gray", size=0.8)+
   geom_point(data=first_freeze_yday, aes(y=year, x=as.Date(yday,origin='2020-01-01'))
-             ,fill = "purple",shape = 21, size = 3, color = 'black') +
+             ,fill="purple", shape=21, size=3, color='black') +
   geom_point(data=last_freeze_yday, aes(y=year, x=as.Date(yday,origin='2020-01-01')) 
-             ,fill = "blue",shape = 21, size = 3, color = 'black') +
-  scale_y_reverse(breaks= seq(1950, 2020, 10), minor_breaks = seq(1944, 2020, 2)) +
-  scale_x_date(date_labels = "%b", breaks = "month") +
+             ,fill="blue", shape=21, size=3, color='black') +
+  scale_y_reverse(breaks= seq(1870, 2020, 10), minor_breaks = seq(1870, 2020, 2)) +
+  scale_x_date(date_labels="%b", breaks="month") +
   theme_minimal() +
   theme( plot.title.position = "panel"
           ,plot.title = element_textbox_simple(
@@ -61,7 +62,7 @@ freeze <- ggplot() +
             lineheight = 1,
             padding = margin(5.5, 5.5, 5.5, 5.5),
             margin = margin(0, 0, 5.5, 0))) +
-  ggtitle("<b>Yearly First and Last Freeze Dates for US Zipcode 97218 </b>   <br><span style = 'font-size:14pt'>
+  ggtitle("<b>Annual First and Last Freeze for Portland, OR </b>   <br><span style = 'font-size:14pt'>
            <br><span style = 'color:blue;'>Blue dots indicate the last freeze of winter/spring</span> 
            <br><span style = 'color:purple;'> Purple dots indicate the first freeze of fall/winter.</span> 
            <br><span style = 'color:gray;'> Gray dots indicate days with a min temp at or below 32 degrees Fahrenheit.</span>") +
@@ -72,4 +73,5 @@ freeze <- ggplot() +
 
 freeze
 
-ggsave("Yearly first and last freeze for US zipcode 97218.png", height= 6.5, width = 8, dpi = 300)
+# ggsave("Yearly first and last freeze for US zipcode 97218.png", height= 6.5, width = 8, dpi = 300)
+ggsave("images/Yearly first and last freeze for Portland, OR.png", height= 6.5, width = 8, dpi = 300)
